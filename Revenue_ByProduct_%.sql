1896438bb14da8fbc40484329c8a7e8da4a2b6c1
Ahmed Safwan C <ahmedsafwan626@gmail.com>
2026-03-21 00:37:18 +0530

    Delete Revenue_ByProduct_%.sql

--- Revenue_ByProduct_%.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    c2 AS Product_Name, 
-    SUM(c6) AS Product_Revenue,
-    (SUM(c6) * 100.0 / (SELECT SUM(c6) FROM Ecommerce_Project_Cleaned)) AS Percentage_of_Total_Revenue
-FROM Ecommerce_Project_Cleaned
-GROUP BY c2
-ORDER BY Percentage_of_Total_Revenue DESC
-LIMIT 10;
